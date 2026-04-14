8a34de621b0f6131502170b87c804c88b6e5e92d
heedo <devheedoo@gmail.com>
2026-04-14 11:49:55 +0900

    feat: 채팅 UI 구현 w/ streamlit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import streamlit as st
+
 def main():
-    print("Hello from nc-life-coach!")
+    st.title("Life Coach: Web Search")
+
+    # 메시지 상태 초기화
+    if "messages" not in st.session_state:
+        st.session_state.messages = list()
+
+    # 메시지 표시
+    for msg in st.session_state.messages:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
+
+    # 메시지 입력
+    if prompt := st.chat_input("메시지를 입력하세요..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
 
 if __name__ == "__main__":
